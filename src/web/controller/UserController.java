05f3c9b83991d2419c3f3f06cfe08a3f289cdd6c
Asha <1374018377@qq.com>
2019-04-24 18:17:44 +0800

    向服务器返回执行是否成功

--- src/web/controller/UserController.java
+++ src/web/controller/UserController.java
@@ -2,6 +2,13 @@ package web.controller;
 
 import web.User;
 import web.dao.UserDao;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +21,12 @@ public class UserController {
     UserDao userDao;
 
     @RequestMapping(value="/add.do",method=RequestMethod.POST)
-    public int addUser(User user){
+    public void addUser(User user,HttpServletRequest request, HttpServletResponse response) throws IOException{
+    	request.setCharacterEncoding("utf-8");  
+        response.setContentType("text/html;charset=utf-8"); 
         System.out.println(user.getTime()+":"+user.getUserName());
         userDao.add(user);
-		return 1;
+        response.getWriter().write("{\"falg\":\"success\"}"); 
     }
 
 }
