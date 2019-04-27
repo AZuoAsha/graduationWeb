2f9c5335f800f1966bc2d1a653842545f913535f
Asha <1374018377@qq.com>
2019-04-27 19:37:48 +0800

    增加了日志存储目录Log

--- src/web/controller/UserController.java
+++ src/web/controller/UserController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,7 @@ import com.alibaba.fastjson.JSONObject;
 @Controller
 @RequestMapping(value="/user")
 public class UserController {
+	private static Logger log = Logger.getLogger(UserController.class);
     @Autowired
     UserDao userDao;
 
